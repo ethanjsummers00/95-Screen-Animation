1189783183cbfcbcca82127ae6eb4aae9ae94f26
Ethan <ethanjsummers00@gmail.com>
2022-07-21 23:11:40 -0700

    Start menu
    - Added a start menu rectangle and object toggled by home button press

--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
 let homeOn = false;
 
 function setup() {
-  createCanvas(1820, 900);
+  createCanvas(1920, 1080);
   background(1, 130, 129);
   
   homeButtonOb = new homeButton()
+  menuOb = new menu();
 }
 
 function draw() {
-  // print(mouseX, mouseY);
+  background(1, 130, 129);
 
   rectMode(CORNER);
   fill(150);
-  rect(0, 850, 1820, 50);
+  rect(0, 1031, 1920, 50);
 
   fill(0, 0, 200);
   rect(360, 200, 240, 180);
 
   homeButtonOb.display();
+  menuOb.display();
 }
 
 function mousePressed(){
@@ -26,31 +28,55 @@ function mousePressed(){
 
 class homeButton {
   constructor() {
-    this.x1 = 35;
-    this.x2 = 60;
-    this.y1 = 875;
-    this.y2 = 35;
-    this.size = 50;
+    this.x = 35;
+    this.y = 1056;
+    this.w = 75;
+    this.h = 50;
+    this.size = 45;
   }
 
   display() {
+
     if (homeOn == false){
       rectMode(CENTER);
-      fill(130);
-      rect(this.x1, this.y1, this.x2, this.y2);  
+      fill(96, 140, 0);
+      rect(this.x, this.y, this.w, this.h);
+
     }
-    else {
+    else if (homeOn == true) {
       rectMode(CENTER);
-      fill(13);
-      rect(this.x1, this.y1, this.x2, this.y2);   
+      fill(127, 186, 0);
+      rect(this.x, this.y, this.w, this.h);
+
+      rectMode(CORNER);
+      fill(150);
+      rect(0, 431, 400, 600)
+
     }
   }
 
 
   clicked() {
-    if (dist(mouseX, mouseY, this.x1, this.y1) < this.size) {
+    if (dist(mouseX, mouseY, this.x, this.y) < this.size) {
       if(homeOn == true) homeOn = false;
       else homeOn = true;
   }
+
+  if (mouseX > 400) {
+    homeOn = false;
+  }
   print(homeOn);
-}}
+  print(mouseX, mouseY);
+  }
+}
+
+class menu {
+
+  display() {
+    if (homeOn == true){
+
+    }
+  }
+
+
+}
